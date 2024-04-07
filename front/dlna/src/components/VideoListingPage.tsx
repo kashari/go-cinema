e4d8f9ee2536bc03867c7246f2592acbad669655
Misen Kashari <misenkashari@icloud.com>
2024-04-07 12:18:16 +0200

    onwards a big update

--- front/dlna/src/components/VideoListingPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const VideoListingPage: React.FC = () => {
-  return <div className="container mx-auto p-8">ok</div>;
-};
-
-export default VideoListingPage;
--- front/dlna/src/types/file.ts
+++
@@ -1,4 +0,0 @@
-export interface FileRow {
-  Name: string;
-  Size: string;
-}
